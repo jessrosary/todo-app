e8d2a9cd043b3e8dc9c0db288473011278b5c78f
Jess <jess@pop-os.localdomain>
2022-11-26 13:15:27 -0500

    remove extraneous code

--- src/index.js
+++ src/index.js
@@ -19,23 +19,11 @@ appEl.appendChild(todoList);
 const addTodo = () => {
   let input = textField.value;
   if (input) {
-    // const li = document.createElement('li');
-    // const div = document.createElement('div');
-    // div.innerText = input;
-    // li.appendChild(div);
-    // todoList.appendChild(li);
-    // textField.value = '';
-
     const newTodo = document.createElement('li');
     newTodo.innerText = input;
     newTodo.addEventListener('click', () => {
-      // console.log('click', newTodo.innerText);
       toggleTodo(newTodo);
     });
-
-    // newTodo.addEventListener('click', () => {
-    //   console.log('click', newTodo.innerText);
-    // });
 
     todoList.appendChild(newTodo);
     textField.value = '';
@@ -43,19 +31,6 @@ const addTodo = () => {
     console.log('need input');
   }
 };
-
-// todoList.querySelectorAll('li').forEach((e) => {
-//   e.addEventListener('click', checkTodo);
-// });
-
-// todoList.addEventListener('click', (event) => {
-//   const todo = event.target;
-//   console.log(todo);
-//   todo.style.opacity = '0.5';
-//   todo.style.textDecoration = 'line-through';
-
-//   // console.log(event);
-// });
 
 submitButton.addEventListener('click', addTodo);
 textField.addEventListener('keydown', (e) => {
@@ -71,9 +46,5 @@ const toggleTodo = (todoEl) => {
     todoEl.classList.add('done');
   }
 };
-
-// const checkTodo = () => {
-//   console.log('clicked!');
-// };
 
 console.log('🚀 todo app started. welcome!', { appEl });
