578c22ef96fea4d2277e7f429d64312ce2532ee2
Jess <jess@pop-os.localdomain>
2022-11-25 23:00:59 -0500

    add click handler

--- src/index.js
+++ src/index.js
@@ -10,8 +10,24 @@ appEl.appendChild(headingEl);
 const textField = document.createElement('input');
 const submitButton = document.createElement('button');
 submitButton.innerText = 'Add Todo';
+const todoList = document.createElement('ul');
 
 appEl.appendChild(textField);
 appEl.appendChild(submitButton);
+appEl.appendChild(todoList);
+
+const addTodo = () => {
+  let input = textField.value;
+  if (input) {
+    let newTodo = document.createElement('li');
+    newTodo.innerText = input;
+    todoList.appendChild(newTodo);
+    textField.value = '';
+  } else {
+    console.log('need input');
+  }
+};
+
+submitButton.addEventListener('click', addTodo);
 
 console.log('🚀 todo app started. welcome!', { appEl });
